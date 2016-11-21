f52406ce59d4efe9f1e4cd7da5f70a082b3b663a
palmer <palmerhart@gmail.com>
2016-11-20 21:58:08 -0500

    checkpoint-8-animation complete

---
+++ scripts/landing.js
@@ -0,0 +1,15 @@
+var animatePoints = function () {
+    var points = document.getElementsByClassName('point');
+    
+    var revealPoint = function(indexnum) {
+        points[indexnum].style.opacity = 1;
+        points[indexnum].style.transform = "scaleX(1) translateY(0)";
+        points[indexnum].style.msTransform = "scaleX(1) translateY(0)";
+        points[indexnum].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+    
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);            
+        }           
+    }        
+};
