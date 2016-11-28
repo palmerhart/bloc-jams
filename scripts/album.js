6d94e99fd18f7382f088df14540561560138f56d
palmer <palmerhart@gmail.com>
2016-11-27 20:05:48 -0500

    checkpoint-11-album-view completed

--- scripts/album.js
+++ scripts/album.js
@@ -58,16 +58,14 @@ var createSongRow = function (songNumber, songName, songLength) {
     return template;
 };
 
-// new location for #1 below
-var albumTitle = document.getElementsByClassName('album-view-title')[0];
-var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-var albumImage = document.getElementsByClassName('album-cover-art')[0];
-var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
-    // #1 moved up to global for assignment
-    
+    // #1 
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
     // #2
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -82,17 +80,5 @@ var setCurrentAlbum = function(album) {
 };
 
 window.onload = function() {
-    setCurrentAlbum(albumPicasso);
-    
-    var albums = [albumPicasso, albumMarconi, albumFolsomPrison];
-    var index = 1;
-    albumImage.addEventListener("click", function(event) {
-        setCurrentAlbum(albums[index]);
-        index++;
-        if (index == albums.length) {
-            index = 0;            
-        }
-    });    
-};
-
-
+     setCurrentAlbum(albumPicasso);
+ };
